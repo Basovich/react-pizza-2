2e4503c5438598adc3c2cbc4fbc478455e36c0d5
vadim <frghfqv86>
2023-03-15 21:29:27 +0200

    Moved the functionality to Home page

--- src/components/Pizzas/Pizzas.jsx
+++ src/components/Pizzas/Pizzas.jsx
@@ -1,12 +1,6 @@
-import {useEffect, useRef} from "react";
-import {useNavigate, useLocation} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import qs from "qs";
-import isequal from "lodash.isequal";
+import {useSelector} from "react-redux";
 
-import {selectFilter, setFilters} from "../../redux/slices/filterSlice";
-import {changeCart} from "../../redux/slices/cartSlice";
-import {fetchPizzas, selectPizzas} from "../../redux/slices/pizzasSlice";
+import {selectPizzas} from "../../redux/slices/pizzasSlice";
 
 import {StyledPizzas} from "./StyledPizzas";
 import {Pizza} from "../Pizza/Pizza";
@@ -15,91 +9,7 @@ import {NotFoundPizzas} from "./NotFoundPizzas";
 
 export function Pizzas() {
   const skeletons = [...new Array(4)].map((_, index) => (<SkeletonPizza key={index}/>));
-  const {category, search, sortType} = useSelector(selectFilter);
   const {pizzas, status} = useSelector(selectPizzas);
-  const navigate = useNavigate();
-  const isQueryChanged = useRef(false);
-  const isMounted = useRef(false);
-  const { state } = useLocation();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const pizzas = JSON.parse(localStorage.getItem('pizzas'));
-
-    if (pizzas) {
-      dispatch(changeCart({
-        ...pizzas
-      }))
-    }
-  }, [dispatch])
-
-  useEffect(() => {
-    if (state) {
-      const queryString = qs.stringify({
-        search,
-        sortType,
-        category
-      }, { addQueryPrefix: true });
-
-      const currentParams = {
-        search,
-        sortType,
-        category
-      }
-
-      if (isequal(state, currentParams)) {
-        navigate(`${queryString}`);
-      }
-    }
-  }, [state, sortType, category, search, navigate])
-
-  useEffect(() => {
-    if (isMounted.current) {
-      const queryString = qs.stringify({
-        search,
-        sortType,
-        category
-      }, { addQueryPrefix: true });
-
-      navigate(`${queryString}`);
-    }
-
-    isMounted.current = true;
-  }, [sortType, category, search, navigate])
-
-  useEffect(() => {
-    if (window.location.search) {
-      const params = qs.parse(window.location.search.replace('?', ''));
-      const currentParams = {
-        search,
-        sortType,
-        category: `${category}`
-      }
-
-      if (!isequal(params, currentParams)) {
-        dispatch(setFilters(params));
-        isQueryChanged.current = true;
-      }
-    }
-  }, [dispatch, category, search, sortType])
-
-  useEffect(() => {
-    if (!isQueryChanged.current) {
-      const categoryId = category === 0 ? '' : `&category=${category}`;
-      const searchValue = search ? `&search=${search}` : '';
-      const sort = `sortBy=${sortType}`;
-
-      dispatch(fetchPizzas({
-        sort,
-        categoryId,
-        searchValue
-      }));
-
-      window.scrollTo(0, 0);
-    }
-
-    isQueryChanged.current = false;
-  }, [dispatch, sortType, category, search])
 
   return (
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,104 @@
+import {useEffect, useRef} from "react";
+import {useLocation, useNavigate} from "react-router";
+import {useDispatch, useSelector} from "react-redux";
+import qs from "qs";
+import isequal from "lodash.isequal";
+
+import {selectFilter, setFilters} from "../redux/slices/filterSlice";
+import {changeCart} from "../redux/slices/cartSlice";
+import {fetchPizzas} from "../redux/slices/pizzasSlice";
+
+import {Header} from "../components/Header/Header";
 import {Filters} from "../components/Filters/Filters";
 import {Title} from "../components/Title/Title";
 import {Pizzas} from "../components/Pizzas/Pizzas";
-import {Header} from "../components/Header/Header";
 
 export function Home() {
+  const navigate = useNavigate();
+  const isQueryChanged = useRef(false);
+  const isMounted = useRef(false);
+  const { state } = useLocation();
+  const dispatch = useDispatch();
+  const {category, search, sortType} = useSelector(selectFilter);
+
+  useEffect(() => {
+    const pizzas = JSON.parse(localStorage.getItem('pizzas'));
+
+    if (pizzas) {
+      dispatch(changeCart({
+        ...pizzas
+      }))
+    }
+  }, [dispatch])
+
+  useEffect(() => {
+    if (state) {
+      const queryString = qs.stringify({
+        search,
+        sortType,
+        category
+      }, { addQueryPrefix: true });
+
+      const currentParams = {
+        search,
+        sortType,
+        category
+      }
+
+      if (isequal(state, currentParams)) {
+        navigate(`${queryString}`);
+      }
+    }
+  }, [state, sortType, category, search, navigate])
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        search,
+        sortType,
+        category
+      }, { addQueryPrefix: true });
+
+      navigate(`${queryString}`);
+    }
+
+    isMounted.current = true;
+  }, [sortType, category, search, navigate])
+
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.replace('?', ''));
+      const currentParams = {
+        search,
+        sortType,
+        category: `${category}`
+      }
+
+      if (!isequal(params, currentParams)) {
+        dispatch(setFilters(params));
+        isQueryChanged.current = true;
+      }
+    }
+  }, [dispatch, category, search, sortType])
+
+  useEffect(() => {
+    if (!isQueryChanged.current) {
+      const categoryId = category === 0 ? '' : `&category=${category}`;
+      const searchValue = search ? `&search=${search}` : '';
+      const sort = `sortBy=${sortType}`;
+
+      dispatch(fetchPizzas({
+        sort,
+        categoryId,
+        searchValue
+      }));
+
+      window.scrollTo(0, 0);
+    }
+
+    isQueryChanged.current = false;
+  }, [dispatch, sortType, category, search])
+
   return (
     <>
       <Header />
