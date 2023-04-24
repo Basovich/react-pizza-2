a9c898beef731a98bb1dd5074f2de7f7510ea8c5
vadim <frghfqv86>
2023-04-25 01:50:42 +0300

    fix type buttons

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,17 +1,17 @@
-import { FilterInterface } from '../../redux/filter/types';
+import {FilterInterface} from '../../redux/filter/types';
 
 export type ButtonProps = {
-    $btnType: string
-    type: string
-    children: React.ReactNode | React.ReactNode[]
-    className?: string
-    onClick?: () => void
-  }
-  
-  export type LinkButtonProps = {
-    $btnLinkType: string
-    to: string
-    children: React.ReactNode | React.ReactNode[] | Element[]
-    className?: string
-    state?: FilterInterface
-  }
+  $btnType: string
+  type: 'button' | 'submit' | 'reset'
+  children: React.ReactNode
+  className?: string
+  onClick?: () => void
+}
+
+export type LinkButtonProps = {
+  $btnLinkType: string
+  to: string
+  children: React.ReactNode
+  className?: string
+  state?: FilterInterface
+}
