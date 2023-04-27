ebb0e22d3c6a5bf86fead905628e6f97cae5350e
vadim <frghfqv86>
2023-04-28 02:18:50 +0300

    Typed Filters styles

--- src/components/Filters/StyledFilters.ts
+++ src/components/Filters/StyledFilters.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 import {StyledSort} from '../Sort/StyledSort';
+import { mediaQuery } from '../../utils/media-query';
+
+const {tabletLg} = mediaQuery;
 
 export const StyledFilters = styled.div`
   margin-top: 14px;
   margin-bottom: 20px;
   
-  @media (min-width: 1024px) {
+  @media (min-width: ${tabletLg}) {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -14,9 +17,8 @@ export const StyledFilters = styled.div`
   ${StyledSort} {
     margin-top: 8px;
 
-    @media (min-width: 1024px) {
+    @media (min-width: ${tabletLg}) {
       margin-top: 0;
     }
   }
-}
 `;
